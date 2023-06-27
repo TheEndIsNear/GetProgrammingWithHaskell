476f741a4828ae5b317d98fdc3cde00edd78662f
Matt Willy <git@github.com:TheEndIsNear/GetProgrammingWithHaskell.git>
2023-06-26 19:16:32 -0600

    add types to the functions

--- ch9/examples.hs
+++ ch9/examples.hs
@@ -1,9 +1,12 @@
 import Data.Char(toLower)
+myElem :: (Eq a) => a -> [a] -> Bool
 myElem x xs = filteredLength > 0
   where filteredLength = length $ filter (== x) xs
 
+isPalindrome :: [Char] -> Bool
 isPalindrome xs = (==) sentence reverseSentence
   where sentence = map toLower $ filter (/= ' ') xs
         reverseSentence = reverse sentence
 
+harmonic :: (Fractional a, Enum a) => a -> a
 harmonic n = foldr (+) 0 $ map ((/) 1) [1..n]
