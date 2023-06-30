5bc3f221b031171904e791cbb3b4edcc1d06fca7
Matt Willy <git@github.com:TheEndIsNear/GetProgrammingWithHaskell.git>
2023-06-30 16:31:00 -0600

    add encode for four letter alphabet

--- ch15/ciphers.hs
+++ ch15/ciphers.hs
@@ -5,3 +5,18 @@ rotN alphabetSize c = toEnum rotation
   where halfAphabet = alphabetSize `div` 2
         offset = fromEnum c + halfAphabet
         rotation = offset `mod` alphabetSize
+
+largestCharNumber :: Int
+largestCharNumber = fromEnum (maxBound :: Char)
+
+rotChar :: Char -> Char
+rotChar = rotN sizeOfAlphabet
+  where sizeOfAlphabet = 1 + largestCharNumber
+
+message :: [FourLetterAlphabet]
+message = [L1,L3,L4,L1,L1,L2]
+
+fourLetterAlphabetEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
+fourLetterAlphabetEncoder = map rot4l
+  where alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+        rot4l = rotN alphaSize
