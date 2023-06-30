8c2b436f47f67c284a7dcf587d7de12adcf94f74
Matt Willy <git@github.com:TheEndIsNear/GetProgrammingWithHaskell.git>
2023-06-30 16:39:41 -0600

    encode and decode odd valued length alphabets

--- ch15/ciphers.hs
+++ ch15/ciphers.hs
@@ -20,3 +20,28 @@ fourLetterAlphabetEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
 fourLetterAlphabetEncoder = map rot4l
   where alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
         rot4l = rotN alphaSize
+
+data ThreeLetterAlphabet = Alpha
+                           | Beta
+                           | Kappa deriving (Show,Enum,Bounded)
+
+threeLetterMessage :: [ThreeLetterAlphabet]
+threeLetterMessage = [Alpha,Alpha,Beta,Alpha,Kappa]
+
+threeLetterEncoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
+threeLetterEncoder = map rot3l 
+  where alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+        rot3l = rotN alphaSize
+
+rotNdecoder :: (Bounded a, Enum a) => Int -> a -> a
+rotNdecoder n c = toEnum rotation
+  where halfN = n `div` 2
+        offset = if even n
+                 then fromEnum c + halfN
+                 else 1 + fromEnum c + halfN
+        rotation = offset `mod` n
+
+threeLetterDecoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
+threeLetterDecoder = map rot3lDecoder
+  where alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+        rot3lDecoder = rotNdecoder alphaSize
