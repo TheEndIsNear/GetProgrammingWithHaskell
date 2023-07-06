b50c49779423125431725f021f3164219839f6e3
Matt Willy <git@github.com:TheEndIsNear/GetProgrammingWithHaskell.git>
2023-07-06 08:57:18 -0600

    use moind and semigroup to combine probabilities

--- ch17/composition.hs
+++ ch17/composition.hs
@@ -39,3 +39,48 @@ instance Semigroup Color where
            | all (`elem` [Blue,Yellow,Green]) [a,b] = Green
            | all (`elem` [Red,Yellow,Orange]) [a,b] = Orange
            | otherwise = Brown 
+
+type Events = [String]
+type Probs = [Double]
+
+data PTable = PTable Events Probs
+
+instance Show PTable where
+  show (PTable events probs) = mconcat pairs
+    where pairs = zipWith showPair events probs
+
+instance Semigroup PTable where
+  (<>) ptable1 (PTable [] []) = ptable1
+  (<>) (PTable [] []) ptable2 = ptable2
+  (<>) (PTable e1 p1) (PTable e2 p2) = createPTable newEvents newProbs
+    where newEvents = combineEvents e1 e2
+          newProbs = combineProbs p1 p2
+
+instance Monoid PTable where
+  mempty = PTable [] []
+  mappend = (<>)
+
+createPTable :: Events -> Probs -> PTable
+createPTable events probs = PTable events noramlizedProbs
+  where totalProbs = sum probs
+        noramlizedProbs = map (/ totalProbs) probs
+
+showPair :: String -> Double -> String
+showPair event prob = mconcat [event,"|",show prob,"\n"]
+
+cartCombine :: (a -> b -> c) -> [a] -> [b] -> [c]
+cartCombine func l1 l2 = zipWith func newL1 cycledL2
+  where nToAdd = length l2
+        repeatedL1 = map (replicate nToAdd) l1
+        newL1 = mconcat repeatedL1
+        cycledL2 = cycle l2
+
+combineEvents :: Events -> Events -> Events
+combineEvents = cartCombine combiner 
+  where combiner = \x y -> mconcat [x,"-",y]
+
+combineProbs :: Probs -> Probs -> Probs
+combineProbs = cartCombine (*)
+
+coin :: PTable
+coin = createPTable ["heads","tails"] [0.5,0.5]
