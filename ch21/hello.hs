4dcbdc6b870f65e61abb473771fe40fa9c6cf0ab
Matt Willy <git@github.com:TheEndIsNear/GetProgrammingWithHaskell.git>
2023-07-10 20:20:51 -0600

    use maybe with names to say hello

--- ch21/hello.hs
+++ ch21/hello.hs
@@ -1,3 +1,5 @@
+import qualified Data.Map as Map
+
 helloPerson :: String -> String
 helloPerson name = "Hello" ++ " " ++ name ++ "!"
 
@@ -7,3 +9,15 @@ main = do
   name <- getLine
   let statement = helloPerson name
   putStrLn statement
+
+nameMap :: Map.Map Int String
+nameMap = Map.fromList [(1,"Arnold"),(2,"Steve")]
+
+maybeMain :: Maybe String
+maybeMain = do
+  name1 <- Map.lookup 1 nameMap
+  name2 <- Map.lookup 2 nameMap
+  let person1 = helloPerson name1
+  let person2 = helloPerson name2
+  return (mconcat [person1," ",person2])
+
