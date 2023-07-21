49cb4ec8657d6a5cad2e312a57e8af9152245575
Matt Willy <git@github.com:TheEndIsNear/GetProgrammingWithHaskell.git>
2023-07-20 21:20:20 -0600

    exercises for monad

--- ch30/exercises.hs
+++ ch30/exercises.hs
@@ -3,3 +3,9 @@ readInt = read <$> getLine
 
 printDouble :: Int -> IO ()
 printDouble n = print (n*2)
+
+allFmapM :: Monad m => (a -> b) -> m a -> m b
+allFmapM f x = x >>= (\n -> return (f n))
+
+allApp :: Monad m => m (a -> b) -> m a -> m b
+allApp f x = f >>= (\n -> x >>= (\m -> return (n m)))
