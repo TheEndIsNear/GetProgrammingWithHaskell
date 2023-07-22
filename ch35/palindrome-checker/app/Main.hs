e96d1630a29c118b0377693ff5e62774d2aecffe
Matt Willy <git@github.com:TheEndIsNear/GetProgrammingWithHaskell.git>
2023-07-22 11:40:09 -0600

    convert the project to a palindrome checker

--- ch35/palindrome-checker/app/Main.hs
+++ ch35/palindrome-checker/app/Main.hs
@@ -1,6 +1,13 @@
-module Main (main) where
+module Main(main) where
 
-import Lib
+import Palindrome (isPalindrome) 
+import qualified Data.Text.IO as TIO
 
 main :: IO ()
-main = someFunc
+main = do
+  TIO.putStrLn "Enter a word and I'll let you know if it's a palindrome!"
+  text <- TIO.getLine
+  let response = if isPalindrome text
+                    then "it is!"
+                    else "it's not!"
+  TIO.putStrLn response
