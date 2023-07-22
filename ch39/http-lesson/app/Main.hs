f08edc60f4733f2406c4a973efa2d2a073725dfc
Matt Willy <git@github.com:TheEndIsNear/GetProgrammingWithHaskell.git>
2023-07-22 17:08:54 -0600

    pull data from NOAA website

--- ch39/http-lesson/app/Main.hs
+++ ch39/http-lesson/app/Main.hs
@@ -1,6 +1,40 @@
 module Main (main) where
 
-import Lib
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as LC
+import Network.HTTP.Simple
 
 main :: IO ()
-main = someFunc
+main = do
+  response <- httpLBS request
+  let status = getResponseStatusCode response
+  if status == 200
+    then do
+      print "saving request to file"
+      let jsonBody = getResponseBody response
+      L.writeFile "data.json" jsonBody
+    else print "request failed with error"
+
+myToken :: BC.ByteString
+myToken = "<INSERT TOKEN HERE>" -- replace with your token
+
+noaaHost :: BC.ByteString
+noaaHost = "www.ncdc.noaa.gov"
+
+apiPath :: BC.ByteString
+apiPath = "/cdo-web/api/v2/datasets"
+
+buildRequest :: BC.ByteString -> BC.ByteString -> BC.ByteString
+             -> BC.ByteString -> Request
+buildRequest token host method path = setRequestMethod method
+                                $ setRequestHost host
+                                $ setRequestHeader "token" [token]
+                                $ setRequestPath path
+                                $ setRequestSecure True
+                                $ setRequestPort 443
+                                $ defaultRequest
+
+request :: Request
+request = buildRequest myToken noaaHost "GET" apiPath
