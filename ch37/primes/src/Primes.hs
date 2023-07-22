d4f3420611c0aeaabb71ab8b5a8586e7593f23d7
Matt Willy <git@github.com:TheEndIsNear/GetProgrammingWithHaskell.git>
2023-07-22 13:44:42 -0600

    use sieve of Erastothenes

--- ch37/primes/src/Primes.hs
+++ ch37/primes/src/Primes.hs
@@ -1,6 +1,14 @@
-module Primes
-    ( someFunc
-    ) where
+module Primes where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+sieve :: [Int] -> [Int]
+sieve [] = []
+sieve (nextPrime:rest) = nextPrime : sieve noFactors
+  where noFactors = filter ((/= 0) . (`mod` nextPrime)) rest
+
+primes :: [Int]
+primes = sieve [2 .. 10000]
+
+isPrime :: Int -> Maybe Bool
+isPrime n | n < 0 = Nothing
+          | n >= length primes = Nothing
+          | otherwise = Just (n `elem` primes)
