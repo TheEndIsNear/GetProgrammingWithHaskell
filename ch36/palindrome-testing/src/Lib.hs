09874b5db12f09aac4012e0df43087673ba8489a
Matt Willy <git@github.com:TheEndIsNear/GetProgrammingWithHaskell.git>
2023-07-22 13:24:25 -0600

    setup quicktest prop tests to get an isPalindrom function

--- ch36/palindrome-testing/src/Lib.hs
+++ ch36/palindrome-testing/src/Lib.hs
@@ -3,11 +3,20 @@ module Lib
     , preprocess
     ) where
 
-import Data.Char (isPunctuation)
-import Data.Text as T
+import Data.Char (isPunctuation, isSpace, toLower)
+import qualified Data.Text as T
+
+stripWhiteSpace :: T.Text -> T.Text
+stripWhiteSpace = T.filter (not . isSpace)
+
+stripPunctuation :: T.Text -> T.Text
+stripPunctuation = T.filter (not . isPunctuation)
+
+toLowerCase :: T.Text -> T.Text
+toLowerCase = T.map toLower
 
 preprocess :: T.Text -> T.Text
-preprocess = T.filter (not . isPunctuation)
+preprocess = stripWhiteSpace . stripPunctuation .toLowerCase
 
 isPalindrome :: T.Text -> Bool
 isPalindrome text = cleanText == T.reverse cleanText
--- ch36/palindrome-testing/test/Spec.hs
+++ ch36/palindrome-testing/test/Spec.hs
@@ -1,8 +1,8 @@
 import Lib
 import Test.QuickCheck
-import Test.QuickCheck.Instances
-import Data.Char(isPunctuation)
-import Data.Text as T
+import Test.QuickCheck.Instances()
+import Data.Char(isPunctuation, isSpace, toLower)
+import qualified Data.Text as T
 
 prop_punctuationInvarient :: T.Text -> Bool
 prop_punctuationInvarient text = preprocess text ==
@@ -12,8 +12,20 @@ prop_punctuationInvarient text = preprocess text ==
 prop_reversetInvariant :: T.Text -> Bool
 prop_reversetInvariant text = isPalindrome text == (isPalindrome . T.reverse) text
 
+prop_whitespaceInvarient :: T.Text -> Bool
+prop_whitespaceInvarient text = preprocess text ==
+                                preprocess noWhitespaceText
+  where noWhitespaceText = T.filter (not . isSpace) text
+
+prop_capitalizationInvarient :: T.Text -> Bool
+prop_capitalizationInvarient text = preprocess text ==
+                                    preprocess noCapText
+  where noCapText = T.map toLower text
+
 main :: IO ()
 main = do
   quickCheckWith stdArgs { maxSuccess = 1000} prop_punctuationInvarient
-  quickCheck prop_reversetInvariant
+  quickCheckWith stdArgs {maxSuccess = 1000} prop_reversetInvariant
+  quickCheckWith stdArgs {maxSuccess = 1000}  prop_whitespaceInvarient
+  quickCheckWith stdArgs {maxSuccess = 1000}  prop_capitalizationInvarient
   putStrLn "done!"
