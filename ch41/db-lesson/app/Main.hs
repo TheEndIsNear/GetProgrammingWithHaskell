3a30581eca3ab4759dace65d79db28e09b023b1a
Matt Willy <git@github.com:TheEndIsNear/GetProgrammingWithHaskell.git>
2023-07-22 21:43:31 -0600

    add code for using sqlite

---
+++ ch41/db-lesson/app/Main.hs
@@ -0,0 +1,10 @@
+module Main (main) where
+
+import Control.Applicative
+import Database.SQLite.Simple
+import Database.SQLite.Simple.FromRow
+import Data.Time
+
+
+main :: IO ()
+main = print "db-lesson"
